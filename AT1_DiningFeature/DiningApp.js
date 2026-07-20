cdd00b5aad87006fdbb10842206a72e7adca4d92
abbyandili <abbyandili00517@gmail.com>
2026-07-20 21:04:01 +1000

    new MealBooking object instance

--- AT1_DiningFeature/DiningApp.js
+++ AT1_DiningFeature/DiningApp.js
@@ -1,3 +1,13 @@
 // Import the MealBooking class
 const MealBooking = require('./MealBooking.js');
 
+// Create a new MealBooking object instance
+const sampleBooking = new MealBooking(
+  "STU98765",
+  "Alex Morgan",
+  "2026-07-20",
+  "Dinner (Premium Buffet)",
+  3,
+  "Gluten-Free preference"
+);
+
