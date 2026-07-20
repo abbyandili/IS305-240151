dde631df30a7b46a81a5f687ea74687461430c2f
abbyandili <abbyandili00517@gmail.com>
2026-07-20 20:47:34 +1000

    Create MealBooking class

--- AT1_DiningFeature/MealBooking.js
+++ AT1_DiningFeature/MealBooking.js
@@ -1,1 +1,10 @@
+/*
+Program: Dining Meal Booking Feature
+  Student Name: Abigail ANDILI
+  Student ID: 240151
+  Date: 20 July 2026
+  Description: A JavaScript program demonstrating classes,
+  objects, constructors, private fields and methods.
+  */
 
+ class MealBooking {
